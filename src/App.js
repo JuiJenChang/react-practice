69042393188caf215d00367836f9d4f901048686
Ray Chang <juijenchang@juijende-MacBook-Air.local>
2019-09-25 21:20:39 +0800

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+     color: 'blue',
+    }
+  }
+  
+  change = e =>{
+    this.setState({
+      color: e.target.value
+    });
+    console.log(this.state.color)
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="content">
+          <div className="main" style={{backgroundColor: this.state.color}}>
+            <h1>Hello, World!!</h1>
+            <p>What color?</p>
+              <select value={this.state.color} onChange={this.change}>
+                <option value="blue">Blue</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="brown">Brown</option>
+                <option value="purple">Purple</option>
+              </select>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
