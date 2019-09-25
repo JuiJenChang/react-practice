ab0351418042a23c3a598e123d9feae3fff0fe18
Ray Chang <juijenchang@juijende-MacBook-Air.local>
2019-09-26 01:06:17 +0800

    update0925

---
+++ src/component/backgroundSelect.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './background.css';
+
+class Background extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: 'blue',
+        }
+    }
+
+    change = e => {
+        this.setState({
+            color: e.target.value
+        });
+        console.log(this.state.color)
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="content">
+                    <div className="main" style={{ backgroundColor: this.state.color }}>
+                        <h1>Hello, World!!</h1>
+                        <p>What color?</p>
+                        <select value={this.state.color} onChange={this.change}>
+                            <option value="blue">Blue</option>
+                            <option value="red">Red</option>
+                            <option value="green">Green</option>
+                            <option value="brown">Brown</option>
+                            <option value="purple">Purple</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Background;
